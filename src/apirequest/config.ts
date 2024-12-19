de01366eb59817ee2d51dd810c96071a80571979
YogeshwarGadde <gadde.yogeshwar2509@gmail.com>
2024-12-19 16:38:37 +0530

    AddeD

--- src/apirequest/config.ts
+++ src/apirequest/config.ts
@@ -1,4 +1,4 @@
 const env:any = "Production";
 
-export const origin = env == "dev" ? "http://localhost:3000": "https://apinapa.yogeshwargadde.com/"
+export const origin = env == "dev" ? "http://localhost:3000": "https://apinapa.yogeshwargadde.com"
 
