bd1446b88f87bf67b0e2af448f31e03715b8b7ea
YogeshwarInnovationEcoSolutions <Yogeshwar.Gadde@innovationecosolutions.com>
2025-04-01 00:16:35 +0530

    Added

--- src/apirequest/config.ts
+++ src/apirequest/config.ts
@@ -1,5 +1,5 @@
-const env:any = "dev";
+const env:any = "production";
 
-export const origin = env == "dev" ? "http://localhost:3000": "https://apinapa.yogeshwargadde.com"
+export const origin = env == "dev" ? "http://localhost:3000": "https://api.napausa.org"
 
 export const adminCookie = "faf61ac9d4fdb0000025bdf7375e4fc9020000"
