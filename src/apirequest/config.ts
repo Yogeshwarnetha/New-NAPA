547c10d057a1c474eecdadf44cb021dff718bddf
Yogeshwarnetha <gadde.yogeshwar@gmail.com>
2026-05-17 16:34:04 +0530

    Added

--- src/apirequest/config.ts
+++ src/apirequest/config.ts
@@ -1,5 +1,5 @@
 const env: any = "Production";
 
-export const origin = env == "dev" ? "http://localhost:3000" : "https://api.napausa.org"
+export const origin = env == "Production" ? "http://localhost:3000" : "https://api.napausa.org"
 
 export const adminCookie = "faf61ac9d4fdb0000025bdf7375e4fc9020000"
